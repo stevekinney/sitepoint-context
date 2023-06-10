2ae9012c3bd9ac89b4323b5b8bf5dea15c433e7e
Steve Kinney <hello@stevekinney.net>
2023-06-10 14:05:57 -0600

    Fix build issues

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: './src/index.ts',
+      entry: './src/index.tsx',
       name: pkg.name,
       fileName: 'index',
     },
